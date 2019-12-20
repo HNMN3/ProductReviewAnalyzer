8283f3303f8dfc147ace573ad32939a83baee78f
Nitin Kumar Chetwani <hnmn3.nitin@gmail.com>
2019-12-20 15:44:12 +0530

    Change engine creation command

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # NLP_Review_Analyzer
 - Download the private key for your account from GCP and put it in env variable as below:
     export GOOGLE_APPLICATION_CREDENTIALS="/home/hnmn3/NLP_Review_Analyzer/private_key.json"
+- setup the DATABASE_URI as follow:
+    export DATABASE_URI="postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
 - Make sure that python 3.7 is installed
 - Installed the required libraries via below command:
     pip install -r requirements.txt
--- config.py
+++ config.py
@@ -1,9 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///database/review_database.db')
-Session = sessionmaker(bind=engine)
-
-Base = declarative_base()
-
+try:
+    database_uri = os.environ['DATABASE_URI']
+    engine = create_engine(database_uri)
+    Session = sessionmaker(bind=engine)
+    Base = declarative_base()
+except:
+    print("DATABASE_URI env variable not set aborting!!")
+    quit()
